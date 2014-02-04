62cb4beb13476c8668ea2a15e34d9cd345f8cf4a
titanium-iridium <4android@mail.ru>
2014-02-04 20:58:36 +0200

    working

--- script.js
+++ script.js
@@ -1,56 +1,82 @@
 /*global jQuery, $ */
 
-if (!window.File || !window.FileReader) {
-	alert("Your browser have no support of file reading API.");
-} else {
-	var reader = new FileReader();
-	reader.onload = function(event) {
-		var contents = event.target.result;
-		console.log("Содержимое файла: " + contents);
-	};
+var drag = null; // will point to currently dragged item
 
-	reader.onerror = function(event) {
-		console.error("Файл не может быть прочитан! код " + event.target.error.code);
-	};
 
-//	var file = new File("./data.json");
-//	reader.readAsText(file);
-
-	$.get('/data.json', function(data) {
-		var all = JSON.parse(data);
+// populate task table and prepare it for interaction
+var populate = function(data) {
 //		console.log(all);
-		if (!all || !all.length) return;
-		var i, status, k, item,
-			cellClass = ' class="first"',
-			$table = $('<table></table>'),
-			$headRow = $('<tr></tr>'),
-			$dataRow = $('<tr></tr>'),
-			$td;
-		for (i = 0; i < all.length; i++) {
-			status = all[i];
-			$('<th' + cellClass + '>' + status.name + '</th>').appendTo($headRow);
-			$td = $('<td' + cellClass + '></td>');
-			for (k = 0; k < status.items.length; k++) {
-				item = status.items[k];
-				console.log(item);
+	if (!data || !data.length) return;
 
-				$td.append('<div class="item '+ item.type + '">'
-					+ '<div class="id">' + item.id + '</div>'
-					+ '<div class="name">' + item.name + '</div>'
-					+ '</div>'
-				);
+	var i, status, k, item,
+		cellClass = ' class="first"',
+		$table = $('<table></table>'),
+		$headRow = $('<tr></tr>'),
+		$dataRow = $('<tr></tr>'),
+		$td;
+
+	for (i = 0; i < data.length; i++) { // per-status cycle
+		status = data[i];
+		$('<th' + cellClass + '>' + status.name + '</th>').appendTo($headRow);
+		$td = $('<td' + cellClass + '></td>');
+
+		for (k = 0; k < status.items.length; k++) { // per-task cycle
+			item = status.items[k];
+//			console.log(item);
+			$td.append('<div class="item '+ item.type + '"><div class="handle"></div><div class="content">'
+				+ '<div class="param id">' + item.id + '</div>'
+				+ '<div class="param name">' + item.name + '</div>'
+				+ '</div>'
+			);
 
 //				$column.attr('class', item.type);
-				$td.appendTo($dataRow);
-			}
-
-			cellClass = '';
+			$td.appendTo($dataRow);
 		}
 
-		$headRow.appendTo($table);
-		$dataRow.appendTo($table);
-		$table.appendTo('body');
+		cellClass = '';
+	}
+
+	$headRow.appendTo($table);
+	$dataRow.appendTo($table);
+	$table.appendTo('body');
+
+//	$table.on('mousedown', function(e) {
+//		var $target = $(e.target);
+//		if (!$target.is('.handle')) return;
+//		// start drag operation
+//		drag = $target.closest('.item');
+//		console.log(drag);
+//		e.preventDefault();
+//	}).on('mouseup', function(e) {
+//		if (!$(e.target).is('.handle')) return;
+//		// finish drag operation
+//		drag = null;
+//	});
 
 
+
+//	$('.item').draggable({handle:'.handle'});
+
+
+	$('.item').each(function() {
+		new Draggable(this, $('.handle', $(this)).get(0));
 	});
-}
+
+	$('td').each(function() {
+		new DropTarget(this);
+	});
+
+	$table.on('click', function(e) {
+		var $target = $(e.target);
+		if ($target.is('.param')) {
+			$('.editable').attr('contenteditable', 'false').removeClass('editable');
+			$target.attr('contenteditable', 'true').addClass('editable');
+		}
+	});
+
+
+};
+
+$.getJSON('./data.json', populate);
+
+
